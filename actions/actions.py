8439b7751b48d4fa800e10f77e0b230719563a33
Shreyas Ramachandran <shreyasramachandran@gmail.com>
2021-02-10 19:30:38 +0530

    family based search

--- actions/actions.py
+++ actions/actions.py
@@ -17,11 +17,7 @@ import os
 
 translator = Translator(service_urls=['translate.googleapis.com'])
 
-class ActionLanguageSearch(Action):
-    # def __init__(self) -> None:
-    #     self.translator = Translator(service_urls=['translate.googleapis.com'])
-    #     super().__init__()
-    
+class ActionLanguageSearch(Action):    
     def name(self) -> Text:
         return "action_lang_search"
 
@@ -31,14 +27,12 @@ class ActionLanguageSearch(Action):
 
         data_path = os.path.join("data", "cldf-datasets-wals-014143f", "cldf", "languages.csv")
         wals_data = pd.read_csv(data_path)
-        entities = list(tracker.get_latest_entity_values("language"))
+        lang_entities = list(tracker.get_latest_entity_values("language"))
 
-        if len(entities) > 0:
-            print(entities)
-            query_lang = entities.pop()
+        if len(lang_entities) > 0:
+            query_lang = lang_entities.pop()
             query_lang = translator.translate(query_lang, dest='en').text
             query_lang = query_lang.lower().capitalize()
-            print(query_lang)
             
             out_row = wals_data[wals_data["Name"] == query_lang].to_dict("records")
 
@@ -48,6 +42,38 @@ class ActionLanguageSearch(Action):
                 family_hindi = translator.translate(out_row["Family"], dest='hi').text
                 genus_hindi = translator.translate(out_row["Genus"], dest='hi').text
                 out_text = "भाषा का नाम, परिवार, उपपरिवार और आईएसओ कोड %s, %s, %s और %s क्रमशः" % (name_hindi, family_hindi, genus_hindi, out_row["ISO_codes"])
+                dispatcher.utter_message(text = out_text)
+            else:
+                dispatcher.utter_message(text = "माफ़ करना! हमारे पास भाषा %s के रिकॉर्ड नहीं हैं" % query_lang)
+
+        return []
+
+
+
+class ActionFamilySearch(Action):    
+    def name(self) -> Text:
+        return "action_family_search"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        data_path = os.path.join("data", "cldf-datasets-wals-014143f", "cldf", "languages.csv")
+        wals_data = pd.read_csv(data_path)
+        lang_entities = list(tracker.get_latest_entity_values("language"))
+
+        if len(lang_entities) > 0:
+            query_lang = lang_entities.pop()
+            query_lang = translator.translate(query_lang, dest='en').text
+            query_lang = query_lang.lower().capitalize()
+            
+            out_row = wals_data[wals_data["Name"] == query_lang].to_dict("records")
+
+            if len(out_row) > 0:
+                out_row = out_row[0]
+                name_hindi = translator.translate(out_row["Name"], dest='hi').text
+                family_hindi = translator.translate(out_row["Family"], dest='hi').text
+                out_text = "भाषा का नाम और परिवार, %s %s हैं" % (name_hindi, family_hindi)
                 dispatcher.utter_message(text = out_text)
             else:
                 dispatcher.utter_message(text = "माफ़ करना! हमारे पास भाषा %s के रिकॉर्ड नहीं हैं" % query_lang)
